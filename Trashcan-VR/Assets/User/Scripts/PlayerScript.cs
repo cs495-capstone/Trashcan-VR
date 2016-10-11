1122edf073a7e40b7e2b12cd941f24669e34be83
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-10-11 18:45:51 -0400

    Added player script

---
+++ Trashcan-VR/Assets/User/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PlayerScript : MonoBehaviour
+{
+	public GameObject prefab;
+
+    private SteamVR_TrackedObject trackedObj;
+    private FixedJoint joint;
+	private bool connected;
+	private GameObject go;
+
+
+    void Start()
+    {
+		connected = false;
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (!connected && device.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			go = GameObject.Instantiate (prefab);
+			go.transform.position = trackedObj.transform.position;
+			connected = true;
+		} else if (connected) {
+			go.transform.position = trackedObj.transform.position;
+		}
+
+		if (!device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			connected = false;
+		}
+    }
+}
