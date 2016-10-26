647bc3e1f57c9d0220b45dc320d06989a581bafe
Anthony Benavente <ambenavente1@catamount.wcu.edu>
2016-10-25 23:19:48 -0400

    Fixing bad merge
    This reverts commit 0f522439ed5d47a6a7c38f0ec75d9a78573b3e12, reversing
    changes made to b51aa7166966265d10ad130de10e224fde783465.

--- Trashcan-VR/Assets/User/Scripts/Teleport_demo.cs
+++ Trashcan-VR/Assets/User/Scripts/Teleport_demo.cs
@@ -1,35 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
-/// <summary>
-/// This class is used to change the position of an object based on the location of an indicator.
-/// </summary>
+
 public class Teleport_demo : MonoBehaviour {
-	/// <summary>
-	/// The indicator is an object that holds the location of where a user is looking
-	/// </summary>
+
 	public GameObject indicator;
 
-	/// <summary>
-	/// This is the keycode for the teleport key.
-	/// </summary>
-	public KeyCode code;
-
-	/// <summary>
-	/// This method finds the indicator object in the world and pulls it in.
-	/// </summary>
+	// Use this for initialization
 	void Start () {
 		if (indicator == null) {
 			indicator = GameObject.Find ("indicator");
 		}
-		code = KeyCode.T;
 	}
 
-	/// <summary>
-	/// This method checks to see if the teleport key is down, and if so the players position
-	/// becomes the postiion of the indicator.
-	/// </summary>
+	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(code)) {
+		if (Input.GetKeyDown(KeyCode.T)) {
 			foreach (var obj in GetComponentsInChildren<GameObject>
 			this.transform.position = new Vector3(indicator.transform.position.x,this.transform.position.y,indicator.transform.position.z);
 		}
